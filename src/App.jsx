6bbe4fcbac048077bc8a638535b9bedd62c13c3d
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-12-01 16:02:20 +0530

    Completed the doc section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
+import Doctors from "./components/Doctors";
 
 const App = () => {
   return (
@@ -20,6 +21,10 @@ const App = () => {
 
         <div id="services">
           <Services />
+        </div>
+
+        <div id="doctors">
+          <Doctors />
         </div>
       </main>
     </div>
---
+++ src/components/Doctors.jsx
@@ -0,0 +1,135 @@
+import React, { useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+
+const Doctors = () => {
+  const data = [
+    {
+      img: "/src/assets/img/doc1.jpg",
+      name: "Dr. Serena Mitchell",
+      specialties: "Orthopedic Surgeon",
+    },
+    {
+      img: "/src/assets/img/doc2.jpg",
+      name: "Dr. Julian Bennett",
+      specialties: "Cardiologist",
+    },
+    {
+      img: "/src/assets/img/doc3.jpg",
+      name: "Dr. Camila Rodriguez",
+      specialties: "Pediatrician",
+    },
+    {
+      img: "/src/assets/img/doc4.jpg",
+      name: "Dr. Victor Nguyen",
+      specialties: "Neurologist",
+    },
+    {
+      img: "/src/assets/img/doc5.jpg",
+      name: "Dr. Ethan Carter",
+      specialties: "Dermatologist",
+    },
+    {
+      img: "/src/assets/img/doc6.jpg",
+      name: "Dr. Olivia Martinez",
+      specialties: "Ophthalmologist",
+    },
+  ];
+
+  const slider = useRef(null);
+
+  const settings = {
+    accessibility: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 pt-16">
+      <div className=" flex flex-col items-center lg:flex-row justify-between mb-10 lg:mb-0">
+        <div>
+          <h1 className=" text-4xl font-semibold text-center lg:text-start">
+            Our Doctors
+          </h1>
+          <p className=" mt-2 text-center lg:text-start">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus,
+            quidem.
+          </p>
+        </div>
+        <div className="flex gap-5 mt-4 lg:mt-0">
+          <button
+            className=" bg-[#d5f2ec] text-backgroundColor px-4 py-2 rounded-lg active:bg-[#ade9dc]"
+            onClick={() => slider.current.slickPrev()}
+          >
+            <FaArrowLeft size={25} />
+          </button>
+          <button
+            className=" bg-[#d5f2ec] text-backgroundColor px-4 py-2 rounded-lg active:bg-[#ade9dc]"
+            onClick={() => slider.current.slickNext()}
+          >
+            <FaArrowRight size={25} />
+          </button>
+        </div>
+      </div>
+      <div className=" mt-5">
+        <Slider ref={slider} {...settings}>
+          {data.map((e, index) => (
+            <div
+              className="h-[350px] text-black rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] mb-2 cursor-pointer"
+              key={index}
+            >
+              <div>
+                <img
+                  src={e.img}
+                  alt="img"
+                  className=" h-56 rounded-t-xl w-full"
+                />
+              </div>
+
+              <div className=" flex flex-col justify-center items-center">
+                <h1 className=" font-semibold text-xl pt-4">{e.name}</h1>
+                <h3 className=" pt-2">{e.specialties}</h3>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Doctors;
